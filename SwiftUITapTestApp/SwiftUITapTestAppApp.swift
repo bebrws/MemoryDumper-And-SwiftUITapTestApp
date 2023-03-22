8cca8a713ea00166799dbeb18c4f22fca25a6b54
Bradley Barrows <bebrws@gmail.com>
2023-03-22 14:22:58 -0700

    Initial Commit

---
+++ SwiftUITapTestApp/SwiftUITapTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITapTestAppApp.swift
+//  SwiftUITapTestApp
+//
+//  Created by Bradley Barrows on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITapTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
